68edd836a93cec733185a9b37954c7a3c924e78e
Andrija <andreccccc12@gmail.com>
2021-01-08 17:04:13 +0100

    Added images for page layout

--- src/components/HomePage/MainSection/MainSection.jsx
+++ src/components/HomePage/MainSection/MainSection.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import * as StyledComponents from './MainSection.styles';
 
 const MainSection = () => {
     return (
         <StyledComponents.Section>
-            <StyledComponents.Article>placeholder</StyledComponents.Article>
-            <StyledComponents.Article>placeholder</StyledComponents.Article>
-            <StyledComponents.Article>placeholder</StyledComponents.Article>
-            <StyledComponents.Article>placeholder</StyledComponents.Article>
+            <Link to='/'>
+                <StyledComponents.Article>
+                    <StyledComponents.Image src={'/images/headphones-demo2.webp'}/>
+                    <StyledComponents.SectionText>Slusalice</StyledComponents.SectionText>
+                </StyledComponents.Article>
+            </Link>
+            <Link to='/'>
+                <StyledComponents.Article>
+                    <StyledComponents.Image src={'/images/smart-bracelet-demo.jpg'}/>
+                    <StyledComponents.SectionText>Narukvice</StyledComponents.SectionText>
+                </StyledComponents.Article>
+            </Link>
+            <Link to='/'>
+                <StyledComponents.Article>
+                    <StyledComponents.Image src={'/images/charger-demo.webp'}/>
+                    <StyledComponents.SectionText>Punjaci</StyledComponents.SectionText>
+                </StyledComponents.Article>
+            </Link>
+            <Link to='/'>
+                <StyledComponents.Article>
+                    <StyledComponents.Image src={'/images/joystick-demo.webp'}/>
+                    <StyledComponents.SectionText>Dzojstici</StyledComponents.SectionText>
+                </StyledComponents.Article>
+            </Link>
         </StyledComponents.Section>
     );
 }
--- src/components/HomePage/MainSection/MainSection.styles.js
+++ src/components/HomePage/MainSection/MainSection.styles.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const Section = styled.section `
-    border: 2px solid red;
     width: 100%;
     height: 100%;
     text-align: center;
@@ -10,15 +9,40 @@ export const Section = styled.section `
 export const Article = styled.article `
     width: 46%;
     margin: 10px;
-    padding: 10px;
     box-sizing: border-box;
-    border: 2px solid green;
     height: 450px;
     display: inline-block;
-    background-color: #000;
-    color: #fff;
+    background-color: #fff;
+    overflow: hidden;
+    position: relative;
+    border-radius: 20px;
 
     @media (max-width: 604px) {
         width: 90%;
+    }
+`;
+
+export const Image = styled.img `
+    width: 100%;
+    height: 100%;
+    transition: 1s;
+
+    &:hover {
+        transform: scale(1.1) rotate(5deg);
+    }
+`;
+
+export const SectionText = styled.h3 `
+    color: #fff;
+    letter-spacing: 3px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-family: Verdana, sans-serif;
+    transition: .6s;
+
+    &:hover {
+        letter-spacing: 6px;
     }
 `;
