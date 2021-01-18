ee4aa6776fc5b65db0a28f87980237049ca332c6
Andrija <andreccccc12@gmail.com>
2021-01-18 10:25:22 +0100

    Added cart support for joysticks route

--- src/components/Dzojstici/Katalog/Article.component.jsx
+++ src/components/Dzojstici/Katalog/Article.component.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Proizvod, Image, H4, FaShoppingBasketStyled } from '../../KatalogStyles/Katalog.styles';
 
 const Article = ({details}) => {
+    useEffect(() => {
+        if(!localStorage.getItem('dzojstici')) {
+            localStorage.setItem('dzojstici', []);
+        }
+    }, []);
+
     return (
         <Proizvod>
             <Image src={details.imgSrc} alt={details.model}/>
@@ -11,7 +17,24 @@ const Article = ({details}) => {
             <H4>Snaga: {details.snaga}</H4>
             <H4>Tip: {details.tip}</H4>
             <H4>Cena: {details.cena}</H4>
-            <FaShoppingBasketStyled size={35} title='Dodaj u korpu'/>
+            <FaShoppingBasketStyled 
+                onClick={() => {
+                    const niz = localStorage.getItem('dzojstici') || [];
+                    console.log("Niz je ", niz);
+                    let vecUbacen = false;
+                    niz.map(dzojstik => {
+                        if(JSON.stringify(details) === JSON.stringify(dzojstik)) {
+                            alert('Ovaj artikal je vec dodat u korpu, ako zelite da kupite vise od 1 mozete podesiti kolicinu pri narucivanju.');
+                            vecUbacen = true;
+                        }
+                    });
+                    if(!vecUbacen) {
+                        localStorage.setItem('dzojstici', [localStorage.getItem('dzojstici'), details]);
+                    }
+                }}
+                size={35} 
+                title='Dodaj u korpu'
+            />
         </Proizvod>
     );
 };
